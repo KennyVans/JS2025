3b549dc1353c2bd914e5c6c22be6b813bc505be9
KennyVans <vansauskajtekaterina@gmail.com>
2025-03-07 20:20:50 +0200

    homework_3.1

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// строка, число, логичное значение, не иниц., объект, массив, нулл.
+let str = "Hello";
+let numb = 27;
+let IsTrue = true;
+let undf;
+let odj = { name: "Kenny" };
+let arr = [1, 2, 3];
+let nthng = null;
+
+console.log(typeof str);
+console.log(typeof numb);
+console.log(typeof IsTrue);
+console.log(typeof undf);
+console.log(typeof odj);
+console.log(typeof arr);
+console.log(typeof nthng);
