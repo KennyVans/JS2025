d227cb43d82adca944dd41d941092dc7fa0a3659
KennyVans <vansauskajtekaterina@gmail.com>
2025-03-08 10:13:14 +0200

    email,login,password

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
 // строка, число, логичное значение, не иниц., объект, массив, нулл.
-let str = "Hello";
-let numb = 27;
-let IsTrue = true;
-let undf;
-let odj = { name: "Kenny" };
-let arr = [1, 2, 3];
-let nthng = null;
+// let str = "Hello";
+// let numb = 27;
+// let IsTrue = true;
+// let undf;
+// let odj = { name: "Kenny" };
+// let arr = [1, 2, 3];
+// let nthng = null;
 
-console.log(typeof str);
-console.log(typeof numb);
-console.log(typeof IsTrue);
-console.log(typeof undf);
-console.log(typeof odj);
-console.log(typeof arr);
-console.log(typeof nthng);
+// console.log(typeof str);
+// console.log(typeof numb);
+// console.log(typeof IsTrue);
+// console.log(typeof undf);
+// console.log(typeof odj);
+// console.log(typeof arr);
+// console.log(typeof nthng);
+
+let login = prompt ("Username");
+let password = prompt ("Your Password");
+let email = prompt ("Your Email");
+
+console.log (" ${email} ${login} ${password} ");
