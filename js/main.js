f833b2d3c9f0e16a82638e2ad2939fe82a0483fb
KennyVans <vansauskajtekaterina@gmail.com>
2025-03-13 17:55:05 +0200

    дз4.3

--- js/main.js
+++ js/main.js
@@ -32,18 +32,76 @@
 // alert ( "Hello, " + yourName + "! " + "How are you?");
 
 // 4.2 Дано трехзначное число, которое предоставляет пользователь, нужно определить: Верно ли, что все цифры одинаковы? Есть ли среди цифр цифры одинаковые?
-const enterNumber = prompt ("Пожалуйста! Введите 3 числа!");
-// важно! писать без пробела между переменной и []
-const num0 = enterNumber[0];  
-const num1 = enterNumber[1];
-const num2 = enterNumber[2];
-if (num0 === num1 && num1 === num2) {
-    alert ("Все цифры одинаковые!");
-}
-else if (num0 === num1 || num1 === num2 || num0 === num2) {
-    alert ("Есть одинаковые цифры!");
-}
-else {
-    alert("Все цифры разные!");
-}
+// const enterNumber = prompt ("Пожалуйста! Введите 3 числа!");
+// // важно! писать без пробела между переменной и []
+// const num0 = enterNumber[0];  
+// const num1 = enterNumber[1];
+// const num2 = enterNumber[2];
+// if (num0 === num1 && num1 === num2) {
+//     alert ("Все цифры одинаковые!");
+// }
+// else if (num0 === num1 || num1 === num2 || num0 === num2) {
+//     alert ("Есть одинаковые цифры!");
+// }
+// else {
+//     alert("Все цифры разные!");
+// }
 
+// 4.3 
+// год рождения
+function userAge() {
+    const yourBD = prompt ("Ваш год рождения?");
+    const thisYear = new Date().getFullYear();
+    if(yourBD) {
+        const yourAge = thisYear - Number(yourBD);
+        alert(`Вам ${yourAge} лет`);
+    }
+    else {
+        alert("Жаль,  что Вы не захотели ввести Ваш год рождения!"); 
+    }
+}
+userAge();
+// город
+function userCity() {
+    const yourCity = prompt ("Ваш город проживания?");
+    if(yourCity) {  
+        if (yourCity === "Киев" || yourCity === "Київ" || yourCity === "Kyiv") {
+            alert(`Вы живете в столице Украины!`);
+        } 
+        else if (yourCity === "Лондон") {
+            alert("Ты живешь в столице Великобритании!");
+        }
+        else if (yourCity === "Вашингтон") {
+            alert("Ты живеш в столице США!");
+        }
+        else {
+            alert(`${yourCity} - Ваш город!`); 
+        } 
+    }
+    else {
+        alert("Жаль, что Вы не захотели ввести Ваш город!"); 
+    } 
+}
+userCity(); 
+// спорт
+function userSport(){
+    const yourSport = prompt ("Твой любимый вид спорта?");
+    if(yourSport) {
+        if (yourSport === "Бокс") {
+            alert("Круто! Хочешь быть как Виталий Кличко?");
+        }
+        else if(yourSport === "Хоккей") {
+            alert("Круто! Хочешь быть как Уейн Гретцки?");
+        }
+        else if(yourSport === "Баскетбол") {
+            alert("Круто! Хочешь быть как Джеймс Леброн?");
+        }
+        else {
+            alert(`${yourSport} - твой любимый вид спорта!`); 
+        } 
+    }
+    else{
+        alert("Жаль, что Вы не захотели ввести Ваш любимый вид спорта!");
+    }
+}
+userSport();
