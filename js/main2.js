0653ab6c2ed77e79bf6319e061344f4f6c305e2a
KennyVans <vansauskajtekaterina@gmail.com>
2025-03-20 19:43:52 +0200

    h-w_6.3

--- js/main2.js
+++ js/main2.js
@@ -5,3 +5,12 @@ function remove(text, charRemove) {
 let removeSomeText = prompt ("введите текст");
 let removeSomeChar = prompt ("введите символы для удаления");
 alert(remove(removeSomeText, removeSomeChar.split('')));
+// hw_6.2
+
+// дз 6.3
+function removeAllElements(array, item) {
+    return array.filter(element => element !== item);
+}
+let array = [1, 3, 4, 6, 4, 2, 5, 7];
+let arrayNew = removeAllElements(array, 4);
+console.log(arrayNew); 
