c1434314e66640535b30ef76d5605bf5d2bf293a
KennyVans <vansauskajtekaterina@gmail.com>
2025-03-20 22:09:08 +0200

    hw_6.2

--- js/main2.js
+++ js/main2.js
@@ -1,16 +1,24 @@
-// дз 6.1
-function remove(text, charRemove) {
-    return text.split('').filter(char => !charRemove.includes(char)).join('');
-}
-let removeSomeText = prompt ("введите текст");
-let removeSomeChar = prompt ("введите символы для удаления");
-alert(remove(removeSomeText, removeSomeChar.split('')));
-// hw_6.2
+// // дз 6.1
+// function remove(text, charRemove) {
+//     return text.split('').filter(char => !charRemove.includes(char)).join('');
+// }
+// let removeSomeText = prompt ("введите текст");
+// let removeSomeChar = prompt ("введите символы для удаления");
+// alert(remove(removeSomeText, removeSomeChar.split('')));
 
-// дз 6.3
-function removeAllElements(array, item) {
-    return array.filter(element => element !== item);
+
+// // дз 6.3
+// function removeAllElements(array, item) {
+//     return array.filter(element => element !== item);
+// }
+// let array = [1, 3, 4, 6, 4, 2, 5, 7];
+// let arrayNew = removeAllElements(array, 4);
+// console.log(arrayNew); 
+
+// hw_6.2
+function averageNumbers(arr) {
+    let numbers = arr.filter(n => typeof n === 'number');
+    if (numbers.length === 0) return null;
+    return numbers.reduce((a, b) => a + b, 0) / numbers.length;
 }
-let array = [1, 3, 4, 6, 4, 2, 5, 7];
-let arrayNew = removeAllElements(array, 4);
-console.log(arrayNew); 
+console.log(averageNumbers ([10, 'dog', true, 20, null, 30, 'cat', 40]) );
